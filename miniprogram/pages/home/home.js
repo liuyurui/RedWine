1dfe6b077e92397155d9d376e40a140b46ef56c6
liuyurui <liubw_wy@163.com>
2018-09-17 17:03:34 +0800

    我的

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -108,5 +108,23 @@ Page({
       avatarUrl: options.detail.userInfo.avatarUrl,
       nickName: options.detail.userInfo.nickName,
     })
+    this.goMy()
+  },
+
+  goMy: function() {
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.navigateTo({
+            url: '/pages/my/my',
+          })
+        } else {
+          wx.showToast({
+            title: '请先点击登录',
+          })
+        }
+      }
+    })
   }
 })
